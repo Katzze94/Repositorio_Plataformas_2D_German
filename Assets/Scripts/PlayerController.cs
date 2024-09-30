c5033735820d75d3a1e150dcbc07858cb2900561
Katzze94 <germannavasa2003@gmail.com>
2024-09-30 13:45:31 +0200

    commit_Ultima_sesion
    Animación de ataque, correcion de moviminetos, ajustes de camara

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float characterSpeed = 4.5f;  //"f" para que el numero se entienda el float y solo si es decimal sino no hace falta
 
     [SerializeField] private float jumpForce = 10;
+
+    private bool isAttacking;
     // [SerializeField]:Para que las variables privadas salgan en el inspector
     // Start is called before the first frame update
    
@@ -32,59 +34,113 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        Movement();
+
+       if(Input.GetButtonDown("Jump") && GroundSensor.isGrounded && !isAttacking) //esto es lo mismo que : isAttacking == false
+       
+       {
+            Jump();
+       }
+
+        if(Input.GetButtonDown("Fire1") && GroundSensor.isGrounded && !isAttacking)
+        {
+            Attack();
+        }
+     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+     characterRigidbody.velocity = new  Vector2(horizontalInput * characterSpeed, characterRigidbody.velocity.y); 
+          
+    }
+
+    void Movement()
+    {
+        if(isAttacking)
+        {
+            horizontalInput = 0;
+        }
+        else
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+        }
+        
+         horizontalInput = Input.GetAxis("Horizontal");
 
          //Tambien te puedes ahorra la variable de input de salto poninedo solo en el parentesis de "if" : Input.GetButtonDown("Jump"), esto tambien funciona con otros inputs de botones
         if(horizontalInput < 0)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
+            if(!isAttacking)
+            {
+              transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            
             characterAnimator.SetBool("IsRunning", true);
         }
         else if(horizontalInput > 0)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            if(!isAttacking)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            
             characterAnimator.SetBool("IsRunning", true);
         }
         else
         {
             characterAnimator.SetBool("IsRunning", false);
         }
-        
-        
-        
-        if(Input.GetButtonDown("Jump") && GroundSensor.isGrounded) //no poner punto y coma. "=" asignar valor, "==" comprobar valor //todos los inputs de booleana necesitas un "If"
-        {
-            characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            characterAnimator.SetBool("IsJumping", true);
-        }
-
-    
-   
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+
+    void Jump()
+     {
+        characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        characterAnimator.SetBool("IsJumping", true);   //no poner punto y coma. "=" asignar valor, "==" comprobar valor //todos los inputs de booleana necesitas un "If"
+    }
+    
+
+
+    void Attack()
     {
-        characterRigidbody.velocity = new  Vector2(horizontalInput * characterSpeed, characterRigidbody.velocity.y); 
+        StartCoroutine(AttackAnimation());
+        characterAnimator.SetTrigger("Attack");
     }
 
+    IEnumerator AttackAnimation() //sirver para parar, entre frames
+    {
+        isAttacking = true;
 
+        yield return new WaitForSeconds(0.5f);
+
+        isAttacking = false;
+    }
     
+  
     void TakeDamage()
     {
         healthPoints--;
 
-        if(healthPoints == 0) Die();
+        if(healthPoints <= 0) 
+        {
+            Die();  //resta de 1 en 1 al valor actual
+        }
+
+        else 
+        {
+            characterAnimator.SetTrigger("IsHurt");  
+        }
         
-        characterAnimator.SetTrigger("IsHurt");  //resta de 1 en 1 al valor actual
-       
+
         
              
     }
     
     void Die()
     {
-        characterAnimator.SetBool("IsDead", true);
+        characterAnimator.SetTrigger("IsDead");
         Destroy(gameObject, 0.47f);
     }
     
