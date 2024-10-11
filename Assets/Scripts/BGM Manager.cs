ff7d5e2624e69a28632f3e18b7071bad8458d3da
Katzze94 <germannavasa2003@gmail.com>
2024-10-11 13:39:17 +0200

    commit_corecciones audio despues del patio

--- Assets/Scripts/BGM Manager.cs
+++ Assets/Scripts/BGM Manager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BGMManager : MonoBehaviour
@@ -21,11 +22,26 @@ public class BGMManager : MonoBehaviour
         }
 
         _audioSource = GetComponent<AudioSource>();
-   }
+
+        _audioSource.loop = true;
+        _audioSource.mute = false;
+        _audioSource.volume = 1;
+
+    }
 
    public void PlayBGM(AudioClip clip)
    {
         _audioSource.clip = clip;
         _audioSource.Play();
+   }
+
+   public void StopBGM()
+   {
+    _audioSource.Stop();
+   }
+
+   public void PauseBGM()
+   {
+    _audioSource.Pause();
    }
 }
